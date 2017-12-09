8d6051be0e5dcbcdd53a5f3f7a87d4fd88676559
Antoine Boxho <karagan.app@gmail.com>
2017-12-09 21:11:57 +0100

    conditions

--- contracts/Transactions.sol
+++ contracts/Transactions.sol
@@ -1,3 +1,13 @@
+/*********************************** SOME COMMANDS FOR TESTING ***********************************
+    var contract = Transactions.deployed()
+    var accounts = web3.eth.accounts
+    contract.then(function(instance) {return instance.getClients()})
+    contract.then(function(instance) {return instance.getBalance.call(accounts[0])}).then(function(balance) {return balance.toNumber()})
+    contract.then(function(instance) {return instance.addClientToBank(accounts[1], "Satoshi", "Nakamoto").then(function(success) { return success }) })
+    contract.then(function(instance) {return instance.getClients()})
+    contract.then(function(instance) {return instance.transfer(accounts[0], accounts[1], 100).then(function(success) { return success }) })
+**************************************************************************************************/
+
 pragma solidity ^0.4.4;
 
 contract Transactions {
@@ -21,6 +31,7 @@ contract Transactions {
     address owner;
     mapping (address => uint) clientBalance;
 
+    // constructor
     function Transactions(address _clientId, bytes32 _firstName, bytes32 _lastName) public {
       Client memory newClient = Client(_clientId, _firstName, _lastName);
       arrayOfClients.push(newClient);
@@ -29,45 +40,58 @@ contract Transactions {
     }
 
     function addClientToBank(address _clientId, bytes32 _firstName, bytes32 _lastName) public returns (bool success) {
-        if (address(_clientId) == 0) {
+        if (address(_clientId) != 0) {
+            Client memory newClient = Client(_clientId, _firstName, _lastName);
+            if (_clientId != owner) {
+                clientBalance[_clientId] = 0;
+            }
+            arrayOfClients.push(newClient);
+            return true;
+        } else {
             return false;
         }
-        Client memory newClient = Client(_clientId, _firstName, _lastName);
-        if (_clientId != owner) {
-            clientBalance[_clientId] = 0;
-        }
-        arrayOfClients.push(newClient);
-        return true;
     }
 
     function getBalance(address _clientId) public view returns (uint _balance) {
-        return clientBalance[_clientId];
+        if (address(_clientId) != 0) {
+            if (clientExists(_clientId) == true) {
+                return clientBalance[_clientId];
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
     }
 
     function getClients() public view returns (bytes32[], bytes32[], uint[]) {
-      uint length = arrayOfClients.length;
-      bytes32[] memory firstNames = new bytes32[](length);
-      bytes32[] memory lastNames = new bytes32[](length);
-      uint[] memory balances = new uint[](length);
+        uint length = arrayOfClients.length;
+        bytes32[] memory firstNames = new bytes32[](length);
+        bytes32[] memory lastNames = new bytes32[](length);
+        uint[] memory balances = new uint[](length);
 
-      for (uint i = 0; i < length; i++) {
-        Client memory currentClient = arrayOfClients[i];
-        firstNames[i] = currentClient.firstName;
-        lastNames[i] = currentClient.lastName;
-        balances[i] = getBalance(currentClient.clientId);
-      }
-      return (firstNames, lastNames, balances);
+        for (uint i = 0; i < length; i++) {
+            Client memory currentClient = arrayOfClients[i];
+            firstNames[i] = currentClient.firstName;
+            lastNames[i] = currentClient.lastName;
+            balances[i] = getBalance(currentClient.clientId);
+        }
+        return (firstNames, lastNames, balances);
     }
 
     function clientExists(address _clientId) public view returns (bool) {
-      uint length = arrayOfClients.length;
-      for (uint i = 0; i < length; i++) {
-        Client memory currentClient = arrayOfClients[i];
-        if (currentClient.clientId == _clientId) {
-          return true;
+        if (address(_clientId) != 0) {
+            uint length = arrayOfClients.length;
+            for (uint i = 0; i < length; i++) {
+                Client memory currentClient = arrayOfClients[i];
+                if (currentClient.clientId == _clientId) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            return false;
         }
-      }
-      return false;
     }
 
     function getTransactions() public view returns (address[], address[], uint[]) {
@@ -87,13 +111,17 @@ contract Transactions {
     }
 
     function transfer(address _from, address _to, uint _money) public returns(bool) {
-        if (clientBalance[_from] == 0 || clientBalance[_from] < _money) {
+        if (clientExists(_from) == true || clientExists(_to) == true) {
+            if (clientBalance[_from] == 0 || clientBalance[_from] < _money) {
+                return false;
+            }
+            Transaction memory newTransaction = Transaction(_from, _to, now, _money);
+            clientBalance[_from] -= _money;
+            clientBalance[_to] += _money;
+            arrayOfTransactions.push(newTransaction);
+            return true;
+        } else {
             return false;
         }
-        Transaction memory newTransaction = Transaction(_from, _to, now, _money);
-        clientBalance[_from] -= _money;
-        clientBalance[_to] += _money;
-        arrayOfTransactions.push(newTransaction);
-        return true;
     }
 }
